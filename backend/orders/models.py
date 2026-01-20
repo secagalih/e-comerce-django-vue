f28d21e0e143df34df4f9fa362082f32e98f5bd3
secagalih <ysec0681@gmail.com>
2026-01-20 10:26:50 +0700

    Refactor Order and Product models: Remove total_price field, add unique constraint to OrderItem, and introduce PromotionEvent and Category models

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -16,7 +16,6 @@ class Order(models.Model):
     status= models.CharField(choices=StatusChoices.choices, default=StatusChoices.PENDING)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, through='OrderItem')
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
@@ -28,3 +27,9 @@ class OrderItem(models.Model):
     order = models.ForeignKey(Order, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
     quantity = models.PositiveIntegerField(default=1)
+        
+    class Meta:
+        unique_together = ('order', 'product')
+
+    def __str__(self):
+        return f"OrderItem {self.id} - {self.product.name} - {self.quantity}"
--- backend/products/models.py
+++ backend/products/models.py
@@ -7,23 +7,30 @@ class Product(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
-    image = models.ImageField(upload_to='products/', null=True, blank=True)
     stock = models.IntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    @property
-    def image_url(self):
-        if self.image and hasattr(self.image, 'url'):
-            return self.image.url
-        return None
-      
-    def in_stock(self):
-        return self.stock > 0
-    
     def __str__(self):
         return self.name
     
-  
-        
-        
+class PromotionEvent(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    price_reduction = models.DecimalField(max_digits=10, decimal_places=2)
+    start_date = models.DateTimeField()
+    end_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(unique=True)
+    is_active = models.BooleanField(default=True)
+    level = models.IntegerField(default=0)
+    parent = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
